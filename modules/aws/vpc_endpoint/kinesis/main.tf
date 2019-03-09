b2cc507843d408d9900417beab1e6e6c15a83f40
Hidenori Sugiyama <madogiwa@gmail.com>
2019-03-10 01:37:39 +0900

    fix bug that allow_from_all does not actually worked

--- modules/aws/vpc_endpoint/kinesis/main.tf
+++ modules/aws/vpc_endpoint/kinesis/main.tf
@@ -30,14 +30,14 @@ resource aws_security_group "tag" {
 }
 
 resource aws_security_group "allow" {
-  count = "${var.allow_from_all == true ? 1 : 0}"
+  count = "${var.allow_from_all ? 1 : 0}"
   vpc_id = "${var.vpc_id}"
 
   ingress {
     protocol = "-1"
     from_port = 0
-    to_port = 65535
-    cidr_blocks = ["0.0.0.0"]
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
