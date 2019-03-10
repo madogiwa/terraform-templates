ee29a4a833fda262ec1f3402164807d83bec248f
Hidenori Sugiyama <madogiwa@gmail.com>
2019-03-10 13:56:37 +0900

    add version constraint

--- examples/aws/network/provider-misc.tf
+++ examples/aws/network/provider-misc.tf
@@ -1,5 +1,16 @@
 
-provider "archive" {}
-provider "null" {}
-provider "random" {}
-provider "template" {}
+provider "archive" {
+  version = "~> 1.1"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "random" {
+  version = "~> 2.0"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
