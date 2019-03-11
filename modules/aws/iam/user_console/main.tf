6c2c3ce40791f040ccdb9d58bb7ce7e5dd2e825f
Hidenori Sugiyama <madogiwa@gmail.com>
2019-03-12 01:41:25 +0900

    add allow manage accesskey

--- modules/aws/iam/user_console/main.tf
+++ modules/aws/iam/user_console/main.tf
@@ -59,11 +59,31 @@ data aws_iam_policy_document "mfa" {
     ]
     resources = ["*"]
   }
+}
 
+data aws_iam_policy_document "password" {
   statement {
     effect = "Allow"
     actions = [
-      "iam:ChangePassword"
+      "iam:ChangePassword",
+      "iam:GetLoginProfile"
+    ]
+    resources = [
+      "arn:aws:iam::${data.aws_caller_identity.self.account_id}:user/${var.name}"
+    ]
+  }
+}
+
+data aws_iam_policy_document "access_key" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:CreateAccessKey",
+      "iam:DeleteAccessKey",
+      "iam:GetAccessKeyLastUsed",
+      "iam:GetUser",
+      "iam:ListAccessKeys",
+      "iam:UpdateAccessKey"
     ]
     resources = [
       "arn:aws:iam::${data.aws_caller_identity.self.account_id}:user/${var.name}"
@@ -75,7 +95,25 @@ resource aws_iam_policy "mfa" {
   policy = "${data.aws_iam_policy_document.mfa.json}"
 }
 
+resource aws_iam_policy "password" {
+  policy = "${data.aws_iam_policy_document.password.json}"
+}
+
+resource aws_iam_policy "access_key" {
+  policy = "${data.aws_iam_policy_document.access_key.json}"
+}
+
 resource aws_iam_user_policy_attachment "mfa" {
   user = "${aws_iam_user.instance.name}"
   policy_arn = "${aws_iam_policy.mfa.arn}"
+}
+
+resource aws_iam_user_policy_attachment "password" {
+  user = "${aws_iam_user.instance.name}"
+  policy_arn = "${aws_iam_policy.password.arn}"
+}
+
+resource aws_iam_user_policy_attachment "access_key" {
+  user = "${aws_iam_user.instance.name}"
+  policy_arn = "${aws_iam_policy.access_key.arn}"
 }
